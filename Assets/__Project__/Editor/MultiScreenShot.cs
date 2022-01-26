f7d3219d9394ea72e7e7b21d239a17842c9de59a
İbrahim Nayman <84410079+Ibrahim-nayman@users.noreply.github.com>
2022-01-26 14:16:41 +0300

    Unity editor Update

--- Assets/__Project__/Editor/MultiScreenShot.cs
+++ Assets/__Project__/Editor/MultiScreenShot.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
@@ -590,3 +591,4 @@ namespace MultiScreenshotCaptureNamespace
 		}
 	}
 }
+#endif
