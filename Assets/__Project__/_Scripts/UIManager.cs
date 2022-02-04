b949d6018ceaa101cb5e926e6f0ac4b112fc0559
İbrahim Nayman <84410079+Ibrahim-nayman@users.noreply.github.com>
2022-02-04 14:30:43 +0300

    Add UI manager script, add canvas,slider,image

---
+++ Assets/__Project__/_Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private Transform _endLine;
+    [SerializeField] private Slider _slider;
+
+    [SerializeField] private float _maxDistance;
+
+    public void Start()
+    {
+        _maxDistance = GetDistance();
+    }
+
+    public void Update()
+    {
+        if (_player.position.y <= _maxDistance && _player.position.y <= _endLine.position.y)
+        {
+            float distance = 1 - (GetDistance() / _maxDistance);
+            SetProgress(distance);
+        }
+    }
+
+    public float GetDistance()
+    {
+        return Vector2.Distance(_player.position, _endLine.position);
+    }
+
+    public void SetProgress(float p)
+    {
+        _slider.value = p;
+    }
+}
